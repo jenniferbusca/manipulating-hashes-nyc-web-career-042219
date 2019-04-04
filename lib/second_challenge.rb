61b0b3a2381def921e0e51c5b8e62abab9e0a295
Jennifer Busca <jennifer.busca@gmail.com>
2019-04-04 22:13:30 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -14,3 +14,13 @@
 # end
 # return new_list.flatten
 # end
+
+def second_challenge
+  groceries = {
+   dairy: ["milk", "yogurt", "cheese"],
+   vegetable: ["carrots", "broccoli", "cucumbers"],
+   meat: ["chicken", "steak", "salmon"],
+   grains: ["rice", "pasta"]
+  }
+groceries.values.flatten
+end
