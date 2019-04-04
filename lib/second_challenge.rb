dd7a423ad66c625a4156a329e7185725ba9510de
Jennifer Busca <jennifer.busca@gmail.com>
2019-04-04 22:13:27 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,16 +1,16 @@
-def second_challenge
-  groceries = {
-   dairy: ["milk", "yogurt", "cheese"],
-   vegetable: ["carrots", "broccoli", "cucumbers"],
-   meat: ["chicken", "steak", "salmon"],
-   grains: ["rice", "pasta"]
-  }
+# def second_challenge
+#   groceries = {
+#   dairy: ["milk", "yogurt", "cheese"],
+#   vegetable: ["carrots", "broccoli", "cucumbers"],
+#   meat: ["chicken", "steak", "salmon"],
+#   grains: ["rice", "pasta"]
+#   }
 
-new_list = []
+# new_list = []
 
-groceries.values.each do |list | 
- new_list << list
+# groceries.values.each do |list | 
+# new_list << list
 
-end
-return new_list.flatten
-end
+# end
+# return new_list.flatten
+# end
